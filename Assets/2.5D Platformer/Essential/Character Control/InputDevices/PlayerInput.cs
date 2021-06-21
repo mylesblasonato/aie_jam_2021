62f0f3784c2d2562f82fe625f5de5bb93a72ad76
Myles <mylesblasonato988@hotmail.com>
2021-06-21 13:50:56 +1000

    added

--- Assets/2.5D Platformer/Essential/Character Control/InputDevices/PlayerInput.cs
+++ Assets/2.5D Platformer/Essential/Character Control/InputDevices/PlayerInput.cs
@@ -7,6 +7,7 @@ namespace Roundbeargames
     public class PlayerInput : MonoBehaviour
     {
         public SavedKeys savedKeys;
+        public string[] axisBindings;
 
         void Update()
         {
@@ -15,14 +16,17 @@ namespace Roundbeargames
             //VirtualInputManager.Instance.MoveDown = Input.GetKey(VirtualInputManager.Instance.DicKeys[InputKeyType.KEY_MOVE_DOWN]);
             //VirtualInputManager.Instance.MoveLeft = Input.GetKey(VirtualInputManager.Instance.DicKeys[InputKeyType.KEY_MOVE_LEFT]);
             //VirtualInputManager.Instance.MoveRight = Input.GetKey(VirtualInputManager.Instance.DicKeys[InputKeyType.KEY_MOVE_RIGHT]);
-            VirtualInputManager.Instance.Turbo = (Input.GetAxis("Run") >= 1);
-            VirtualInputManager.Instance.MoveUp = (Input.GetAxis("Vertical") >= 1);
-            VirtualInputManager.Instance.MoveDown = (Input.GetAxis("Vertical") <= -1) || Input.GetKey(VirtualInputManager.Instance.DicKeys[InputKeyType.KEY_JUMP]);
-            VirtualInputManager.Instance.MoveLeft = (Input.GetAxis("Horizontal") <= -1);
-            VirtualInputManager.Instance.MoveRight = (Input.GetAxis("Horizontal") >= 1);            
             //VirtualInputManager.Instance.Jump = Input.GetKey(VirtualInputManager.Instance.DicKeys[InputKeyType.KEY_JUMP]);
-            VirtualInputManager.Instance.Block = Input.GetKey(VirtualInputManager.Instance.DicKeys[InputKeyType.KEY_BLOCK]);
-            VirtualInputManager.Instance.Attack = Input.GetKey(VirtualInputManager.Instance.DicKeys[InputKeyType.KEY_ATTACK]) || Input.GetKey(VirtualInputManager.Instance.DicKeys[InputKeyType.KEY_JUMP]) && !VirtualInputManager.Instance.Turbo;
+            VirtualInputManager.Instance.Turbo = (Input.GetAxis(axisBindings[0]) >= 1) || Input.GetKey(KeyCode.LeftShift);
+            VirtualInputManager.Instance.MoveUp = Input.GetKey(VirtualInputManager.Instance.DicKeys[InputKeyType.KEY_MOVE_UP]);
+            VirtualInputManager.Instance.MoveDown = Input.GetKey(VirtualInputManager.Instance.DicKeys[InputKeyType.KEY_MOVE_DOWN]);
+            VirtualInputManager.Instance.MoveLeft = (Input.GetAxis(axisBindings[2]) <= -1) || Input.GetKey(VirtualInputManager.Instance.DicKeys[InputKeyType.KEY_MOVE_LEFT]);
+            VirtualInputManager.Instance.MoveRight = (Input.GetAxis(axisBindings[2]) >= 1) || Input.GetKey(VirtualInputManager.Instance.DicKeys[InputKeyType.KEY_MOVE_RIGHT]);            
+            VirtualInputManager.Instance.Block = Input.GetKey(VirtualInputManager.Instance.DicKeys[InputKeyType.KEY_BLOCK]) || Input.GetKey(KeyCode.LeftArrow);
+            VirtualInputManager.Instance.Attack = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.DownArrow)
+                || Input.GetKey(VirtualInputManager.Instance.DicKeys[InputKeyType.KEY_ATTACK]) 
+                || Input.GetKey(VirtualInputManager.Instance.DicKeys[InputKeyType.KEY_MOVE_UP]) && !VirtualInputManager.Instance.Turbo
+                || Input.GetKey(VirtualInputManager.Instance.DicKeys[InputKeyType.KEY_MOVE_RIGHT]) && !VirtualInputManager.Instance.Turbo;
         }
     }
 }
